a89557e92b2c409948d14dc3daff95f1f158dcd6
Alos22 <aniriver689@gmail.com>
2022-01-08 11:00:54 -0500

    issue with checking row(undefined)

--- scripts/app.js
+++ scripts/app.js
@@ -20,10 +20,10 @@ let redScore = 12;
 let blackScore = 12;
 let isClicked = false;
 let pieceClicked;
+let nextCell;
 
 
 $cells.click(function () {
-    console.log("im working kind of");
     console.log($(this).attr('data-col') + $(this).attr('data-row') + $(this).attr('data-piece-id'));
     //selecting a piece to move
     if (isClicked === false) {
@@ -33,9 +33,13 @@ $cells.click(function () {
             $(this).toggleClass('highlight');
         }
         $(this).removeAttr('data-piece-id');
+        // nextCell = "";
+        console.log(nextCell)
         //piece is already selected, move to another place
     } else {
         if ($(this).hasClass('playSquares')) {
+            nextCell = $(this)
+            console.log(nextCell)
             if (pieceClicked.hasClass('redPieces')) {
                 pieceClicked.toggleClass('redPieces highlight');
                 $(this).toggleClass('redPieces');
@@ -46,11 +50,57 @@ $cells.click(function () {
             }
             $(this).attr('data-piece-id', pieceClicked.attr('data-piece-id'));
         }
+        checkRow();
+        checkColumn();
+        isPlayableSpace();
+        console.log(`checking again` + nextCell)
+        console.log(`checking if column` + checkColumn());
+        console.log(`checking if row` + checkRow());
+        console.log(`checking if space is playable` + isPlayableSpace());
         isClicked = false;
-        pieceClicked = "";
+        // pieceClicked = "";
     }
     console.log("I am the piece" + " " + pieceClicked)
 });
+
+
+function isPlayableSpace() {
+    if (checkColumn() === true && checkRow() === true) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function checkColumn() {
+    if (nextCell.attr('data-col') === pieceClicked.attr('data-col') - 1) {
+        return true;
+    } else if (nextCell.attr('data-col') === pieceClicked.attr('data-col') + 1) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function checkRow() {
+    let row = parseInt(pieceClicked.attr('data-row'))
+    console.log(`I am the row` + row)
+    console.log(nextCell.attr('data-row') == row + 1)
+    if (pieceClicked.hasClass('redPieces')) {
+        if (nextCell.attr('data-row') == row + 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if (pieceClicked.hasClass('blackPieces')) {
+        if (nextCell.attr('data-row') === pieceClicked.attr('data-row') - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
 
 
 
